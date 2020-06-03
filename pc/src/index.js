a01ead06cba3ad6c34ed81c185e8f7c0711a3147
yangzhenhong <736647835@qq.com>
2020-06-03 19:07:37 +0800

    修改文件

--- pc/src/index.js
+++ pc/src/index.js
@@ -7,9 +7,7 @@ import './index.css';
 import './assets/fonts/iconfont.css';
 // 由于 antd 组件的默认文案是英文，所以需要修改为中文
 // import zhCN from 'antd/es/locale/zh_CN';
-import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
-// import moment from 'moment';
-// import 'moment/locale/zh-cn';
+import 'antd/dist/antd.css';
 
 React.Component.prototype.$ajax = ajax;
 React.Component.prototype.$net = ajax;
